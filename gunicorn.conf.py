4263f4cb1bd0b58f7ae4672cba12281274d26a1f
gentlesoul18 <oyebamijibarakat7@gmail.com>
2024-11-10 07:23:20 +0100

    still testing 2

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,6 +4,7 @@
 import os
 
 
-port = os.environ.get("PORT", 5000)
-timeout = 0
-bind = [f"0.0.0.0:{port}"]
+port = os.environ.get('PORT', 5000)
+bind = f"0.0.0.0:{port}"
+workers = 1
+timeout = 120
